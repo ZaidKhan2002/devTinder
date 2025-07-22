59827730fc37a6c39a723516ac3592e884b86911
Zaid Khan <zk76159@gmail.com>
2025-07-23 00:22:57 +0530

    Express Router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,59 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const bcrypt = require("bcrypt");
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
 const app = express();
-
-const User = require("./models/user")
-const { validateSignup } = require("./utils/validation")
-const { userAuth } = require("./middlewares/userAuth")
-
 app.use(express.json());
 
-
-app.post("/signup", async (req,res) => {
-
-    try {
-        validateSignup(req)
-        const { password } = req.body;
-        const passwordHash = bcrypt.hash(password, 10);
-        const user = new User({
-            firstName, lastName, emailId, password: passwordHash
-        });
-        await user.save();
-        res.send("User Added Successfully")
-    } catch (error) {
-        throw new Error("Error" + error.message);
-    }
-});
-
-app.post("/login", async (req,res) => {
-    try {
-        const { emailId, password } = req.body;
-        const user = await User.findOne({ emailId: emailId });
-        if(!user){
-            throw new Error("Email Id not present");
-        }
-
-        const isValidPassword = await bcrypt.compare(password, user.password);
-        if(!isValidPassword){
-            throw new Error("Password not correct");
-        }else{
-            res.send("Login Successful");
-        }
-    } catch (error) {
-        res.status(400).send("Error" + error.message)
-    }
-})
-
-app.get("/profile", userAuth, async (req, res) => {
-    try {
-        const user = req.user;
-        res.send(user);
-    } catch (error) {
-        res.status(400).send("ERROR" + error.message);
-    }
-})
-
+app.use("/", authRouter);
+app.use("/", profileRouter);
 
 connectDB().then(() => {
     console.log("DataBase connection successful.")
---
+++ backend/src/routes/auth.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const authRouter = express.Router();
+const { validateSignup } = require("../utils/validation")
+const User = require("../models/user")
+const bcrypt = require("bcrypt");
+
+authRouter.post("/signup", async (req,res) => {
+
+    try {
+        validateSignup(req)
+        const { firstName, lastName, emailId, password } = req.body;
+        const passwordHash = bcrypt.hash(password, 10);
+        const user = new User({
+            firstName, lastName, emailId, password: passwordHash
+        });
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 8 * 3600000)
+        });
+        res.json({ message: "User Added successfully!", data: savedUser });
+    } catch (error) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+
+authRouter.post("/login", async (req,res) => {
+    try {
+        const { emailId, password } = req.body;
+        const user = await User.findOne({ emailId: emailId });
+        if(!user){
+            throw new Error("Email Id not present");
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+        if (isPasswordValid) {
+            const token = await user.getJWT();
+      
+            res.cookie("token", token, {
+              expires: new Date(Date.now() + 8 * 3600000),
+            });
+            res.send(user);
+          } else {
+            res.status(400).send("ERROR : " + err.message);
+          }
+    } catch (error) {
+        res.status(400).send("Error" + error.message)
+    }
+})
+
+module.exports = authRouter;
