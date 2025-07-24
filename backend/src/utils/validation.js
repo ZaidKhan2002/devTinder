85d04fd4f50aa081238ac440273be2021d345455
Zaid Khan <zk76159@gmail.com>
2025-07-24 23:40:42 +0530

    more routers

--- backend/src/utils/validation.js
+++ backend/src/utils/validation.js
@@ -2,18 +2,36 @@ const validator = require("validator");
 
 const validateSignup = (req) => {
     const { firstName, lastName, emailId, password } = req.body;
+    if (!firstName || !lastName) {
+      throw new Error("Name is not valid!");
+    } else if (!validator.isEmail(emailId)) {
+      throw new Error("Email is not valid!");
+    } else if (!validator.isStrongPassword(password)) {
+      throw new Error("Please enter a strong Password!");
+    }
+  };
+  
+  const validateEditProfileData = (req) => {
+    const allowedEditFields = [
+      "firstName",
+      "lastName",
+      "emailId",
+      "photoUrl",
+      "gender",
+      "age",
+      "about",
+      "skills",
+    ];
+  
+    const isEditAllowed = Object.keys(req.body).every((field) =>
+      allowedEditFields.includes(field)
+    );
+  
+    return isEditAllowed;
+  };
 
-    if(!firstName || !lastName){
-        throw new Error("Name is not valid");
-    }
-    else if(!validator.isEmail(emailId)){
-        throw new Error("email not valid");
-    }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error("Password not strong");
-    }
-}
 
 module.exports = {
     validateSignup,
+    validateEditProfileData,
 }
