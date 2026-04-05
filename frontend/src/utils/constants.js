2ad9664fa865edb2207e6bba5e5db581e9ee4985
Zaid Khan <zk76159@gmail.com>
2026-04-05 15:31:17 +0530

    Made frontend file changes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,6 @@
 // export const BASE_URL = "/api"
 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7000" : "/api";
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7000" : "/api";
+
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-backend.onrender.com";
+
